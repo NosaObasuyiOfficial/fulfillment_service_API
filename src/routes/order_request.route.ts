b7cde55b7bbf2698b9d70d3516a203e4f6adeffa
Nosa Clement Obasuyi <131542687+NosaObasuyiOfficial@users.noreply.github.com>
2023-10-24 16:22:59 +0100

    fifth commit

--- src/routes/order_request.route.ts
+++ src/routes/order_request.route.ts
@@ -3,7 +3,7 @@ import { fulfillment_order } from '../controllers/update_order_status.controller
 
 const router = express.Router()
 
-router.get('/fulfillment', fulfillment_order)
+router.post('/fulfillment', fulfillment_order)
 
 
 export default router
